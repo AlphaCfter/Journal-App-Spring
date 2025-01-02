593c413777cbec781e01445b50db276dbfda23db
AlphaCfter <aku.ajith.kumar.ms@gmail.com>
2025-01-02 15:52:39 +0530

    User endpoint created using REST

---
+++ src/main/java/com/alphacfter/journalApp/controller/UserEntryController.java
@@ -0,0 +1,58 @@
+package com.alphacfter.journalApp.controller;
+
+import com.alphacfter.journalApp.entity.JournalEntry;
+import com.alphacfter.journalApp.entity.User;
+import com.alphacfter.journalApp.service.JournalEntryService;
+import com.alphacfter.journalApp.service.UserEntryService;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+
+//Request Mapping is the parent of the exposed endpoint soo all end points within
+//the body of this class will proceed via this endpoint
+@RestController
+@RequestMapping("/user")
+public class UserEntryController {
+
+    @Autowired
+    private UserEntryService userService;
+
+    @GetMapping
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
+
+    /**
+     * createUser creates a new user requesting from the user body itself from the frontend (Postman)
+     * @param user is the user variable from the response body(Postman)
+     */
+    @PostMapping
+    public void createNewUser(@RequestBody User user){
+        userService.saveEntry(user);
+    }
+
+    /**
+     * updateUser generates or changes a username and password if required
+     * @param user user field input by an API frontend (Postman)
+     */
+    @PutMapping("/{username}")
+    public ResponseEntity<?> updateUser(@RequestBody User user,@PathVariable String username){
+        User userInDB = userService.findByUsername(username);
+        if(userInDB != null){
+            userInDB.setUsername(user.getUsername());
+            userInDB.setPassword(user.getPassword());
+            userService.saveEntry(userInDB);
+        }
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+
+
+}
---
+++ src/main/java/com/alphacfter/journalApp/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.alphacfter.journalApp.repository;
+
+import com.alphacfter.journalApp.entity.User;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, ObjectId> {
+    User findByUsername(String username);
+}
